bf337f32b5243d1eac7facd8ad3614e75cf0e89a
unknown <guna.rakesr@gmail.com>
2024-12-28 13:31:30 +0530

    full code

---
+++ src/ComingSoon.js
@@ -0,0 +1,21 @@
+// src/ComingSoon.js
+
+import React from 'react';
+//import './ComingSoon.css';
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-container">
+      <div className="coming-soon-content">
+        <h1>Coming Soon!</h1>
+        <p>We are working hard to get our website ready. Stay tuned!</p>
+        <div className="email-signup">
+          <p><b>Phone:</b> 9994583362</p>
+          <p><b>Email:</b> asiancoolingtowers@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;
